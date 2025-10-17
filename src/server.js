b15e3cfb18ae1bceb62bbe78a757a73328916934
Sergey-8057 <erste_sergey@proton.me>
2025-10-18 01:54:02 +0300

    created swagger for contacts

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { UPLOAD_DIR } from './constants/index.js';
 import { getEnvVar } from '../utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ export const setupServer = () => {
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
